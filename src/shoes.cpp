deccdd5ca2b8850f3f92dcf50c4a91fd1749a0d2
agathags <42275153+agathags@users.noreply.github.com>
2019-05-30 12:11:33 -0300

    Create shoes.cpp

---
+++ src/shoes.cpp
@@ -0,0 +1,52 @@
+#include "shoes.h"
+
+
+//Shoes::Shoes(string type, string brand, string color, int size, double price)
+
+
+void set_type(string type) {
+
+
+}
+
+
+void set_brand(string brand) {
+
+
+}
+
+
+void set_color(string color) {
+
+
+}
+
+
+void set_size(int size) {
+
+
+}
+
+
+string get_type() {
+
+
+}
+
+
+string get_brand() {
+
+
+}
+
+
+string get_color() {
+
+
+}
+
+
+int get_size() {
+
+  
+}
