0fb85cdd764056ba7faa1e92093f19cb7c745cfa
agathags <42275153+agathags@users.noreply.github.com>
2019-05-30 12:10:15 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,40 @@
-#include <iostream>
+#include "menu.h"
 
 int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+  
+  bool store_is_open = true; //vai ficar aberta enquanto o user nao pedir pra fechar
+
+  while(store_is_open) {
+
+    int sign_in_or_up; //define se o user quer logar ou cadastrar
+
+    cout << "Bem-vindx ao e-commerce!" << endl
+    << "O que voce gostaria de fazer?" << endl << "1 - Se cadastrar" << endl
+    << "2 - Fazer login" << endl << "3 - Sair da loja";
+    cin >> sign_in_or_up;
+    cout << endl;
+
+    switch(sign_in_or_up) {
+      
+      case 1:
+        sign_up();
+        break;
+      
+      case 2:
+        login();
+        break;
+
+      case 3:
+        store_is_open = false;
+        break;
+
+      default:
+        cout << "Opcao invalida." << endl << endl;
+        break;
+    
+    }
+
+  }
+
+  return 0;
 }
