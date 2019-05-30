2a3e43d9dc106a75c3aec1f2502930fad61da1da
Eduardo Fiuza Cruz <eduardofiuza6@hotmail.com>
2019-05-30 12:26:15 -0300

    Update administrator.cpp

--- src/administrator.cpp
+++ src/administrator.cpp
@@ -1,8 +1,30 @@
+#include <utility>
+
+#include <utility>
+
+#include <utility>
+
+#include <utility>
+
 #include "administrator.h"
 
-Admin::Admin(string name, string login, string password) : User::User(name, login, password) {}
+//CONSTRUCTOR
+Admin::Admin(string name, string login, string password) : User(std::move(name), std::move(login), std::move(password))
+    {};
+
+//GETTERS
+string Admin::get_name(){
+    return this->_name;
+};
+string Admin::get_login(){
+    return this->_login;
+};
+string Admin::get_password(){
+    return this->_password;
+};
 
 
+//ATTRIBUTES
 void add_product_storage() {
 
 //arquivo
@@ -34,5 +56,5 @@ void remove_product() {
 void show_storage() {
 
 //arquivo
-  
+
 }
