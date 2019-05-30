5465fdd483535c934f6ef144b2ff1cd7ed0d5176
Eduardo Fiuza Cruz <eduardofiuza6@hotmail.com>
2019-05-30 12:25:00 -0300

    Update product.h

--- include/product.h
+++ include/product.h
@@ -8,26 +8,30 @@
 using namespace std;
 
 class Product {
+public:
+    double _price;
+    int _quantity;
+    string _type;
+    string _color;
+    string _brand;
+//CONSTRUCTOR
+    Product(double price, int _quantity, string type, string color, string brand);
 
-  double _price;
-  int _quantity;
 
-  public:
-
-    Product(double price);
-
-    void set_price(double price);
-    void set_quantity(int quantity);
+//SETTERS
+    virtual void set_price(double price);
+    virtual void set_quantity(int quantity);
     virtual void set_type(string type);
     virtual void set_color(string color);
     virtual void set_brand(string brand);
 
-    double get_price();
-    int get_qantity();
+//GETTERS
+    virtual double get_price();
+    virtual int get_quantity();
     virtual string get_type();
     virtual string get_color();
     virtual string get_brand();
 
 };
 
-#endif
+#endif //PRODUCT_H_PDS
