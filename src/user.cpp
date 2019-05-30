7602ab8aa3324af863d857ceb0f764c29a68f4d5
agathags <42275153+agathags@users.noreply.github.com>
2019-05-30 12:12:00 -0300

    Create user.cpp

---
+++ src/user.cpp
@@ -0,0 +1,43 @@
+#include "user.h"
+
+
+User::User(string name, string login, string password) {
+    this->_name = name;
+    this->_login = login;
+    this->_password = password;
+}
+
+void set_name(string name) {
+
+
+}
+
+
+void set_login(string login) {
+
+
+}
+
+
+void set_password(string password) {
+
+
+}
+
+
+string get_name() {
+
+
+}
+
+
+string get_login() {
+
+
+}
+
+
+string get_password() {
+
+
+}
