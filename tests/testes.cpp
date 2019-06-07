5eee088085d4adb067a6d201cb0c6cf14a29f3a9
Lucas Silveira <lucas.costa@engineer.com>
2019-06-07 17:17:07 -0300

    Add object creation tests

--- tests/testes.cpp
+++ tests/testes.cpp
@@ -25,15 +25,15 @@ TEST_CASE("Test Admin Creation") {
 }
 
 TEST_CASE("Test User Creation") {
-    CHECK_NOTHROW(User("John Snow", "jsn", "8888!!!@@##$%*"));
-    CHECK_NOTHROW(User("John Snow", "@  jsn", "8888!!!@@##$%*"));
-    CHECK_NOTHROW(User("John Snow__", "@jsn", "8888!!!@@##$%*"));
-    CHECK_NOTHROW(User("John Snow123", "@jsn", "8888!!!@@##$%*"));
-    CHECK_NOTHROW(User("John Snow", "@asdasjsn", "8888!!!@@##$%*"));
-    CHECK_NOTHROW(User("John Snow", "@jsn", "8888!!!@@##$%*"));
-    CHECK_NOTHROW(User("John Snow", "@jsn", "8888!!!@@##$%*"));
-    CHECK_NOTHROW(User("John Snow", "@jsn", "8888!!!@@##$%*"));
-    CHECK_NOTHROW(User("John Snow", "@jsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow", "jsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow", "@  jsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow__", "@jsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow123", "@jsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow", "@asdasjsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow", "@jsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow", "@jsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow", "@jsn", "8888!!!@@##$%*"));
+    CHECK_NOTHROW(Admin("John Snow", "@jsn", "8888!!!@@##$%*"));
 }
 
 TEST_CASE("Test User Creation") {
@@ -48,7 +48,23 @@ TEST_CASE("Test User Creation") {
     CHECK_NOTHROW(Client("John Snow", "@jsn", "8888!!!@@##$%*"));
 }
 
-TEST_CASE("Test User Creation") {
+TEST_CASE("Test Product Creation") {
+    CHECK_NOTHROW(Product(5.0, 2, "cor", "tamanho", "marca"));
+
+}
+
+TEST_CASE("Test Clothes Creation") {
     CHECK_NOTHROW(Clothes("tipo", "marca", "cor", "tamanho", 2.0, 1));
 
 }
+
+TEST_CASE("Test Shoes Creation") {
+    CHECK_NOTHROW(Shoes("tipo", "marca", "cor", 38, 2.0, 1));
+}
+
+TEST_CASE("Test Menu Creation") {
+    CHECK_NOTHROW(Shoes("tipo", "marca", "cor", 38, 2.0, 1));
+}
+
+//cart
+//menu
